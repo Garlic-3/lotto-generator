24b406a759c55137d23a4a9b66d43ac9cdbbc652
minho jung <alsgh846@gmail.com>
2024-11-11 18:18:20 +0900

    asdf
    sadf

--- src/main/java/com/garlic3/lotto_generator/domian/luck/controller/LuckController.java
+++ src/main/java/com/garlic3/lotto_generator/domian/luck/controller/LuckController.java
@@ -1,5 +1,6 @@
 package com.garlic3.lotto_generator.domian.luck.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.garlic3.lotto_generator.domian.luck.dto.YearRequest;
 import com.garlic3.lotto_generator.domian.luck.service.LuckService;
 import lombok.RequiredArgsConstructor;
@@ -20,48 +21,20 @@ public class LuckController {
     private final LuckService luckService;
 
     @GetMapping("/save")
-    public ResponseEntity<String> saveLuck(@RequestBody YearRequest yearRequest) {
-        try {
-            int year = yearRequest.getYear();
+    public ResponseEntity<String> saveLuck(@RequestBody YearRequest yearRequest) throws JsonProcessingException {
+        int year = yearRequest.getYear();
 
+        // Loop through each day of the year
+        for (int month = 1; month <= 12; month++) {
+            for (int day = 1; day <= YearMonth.of(year, month).lengthOfMonth(); day++) {
+                // Format the date as "YYYYMMDD"
+                String date = String.format("%04d%02d%02d", year, month, day);
 
-            // Loop through each day of the year
-            for (int month = 1; month <= 12; month++) {
-                for (int day = 1; day <= YearMonth.of(year, month).lengthOfMonth(); day++) {
-                    // Format the date as "YYYYMMDD"
-                    String date = String.format("%04d%02d%02d", year, month, day);
-
-                    // Call saveHoroscopeDate with the formatted date
-                    luckService.saveLuckData(date);
-                }
+                // Call saveHoroscopeDate with the formatted date
+                luckService.saveLuckData(date);
             }
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
         }
         return ResponseEntity.ok("OK");
     }
 
-    @GetMapping("/save")
-    public ResponseEntity<String> test(@RequestBody YearRequest yearRequest) {
-        try {
-            int year = yearRequest.getYear();
-
-
-            // Loop through each day of the year
-            for (int month = 1; month <= 12; month++) {
-                for (int day = 1; day <= YearMonth.of(year, month).lengthOfMonth(); day++) {
-                    // Format the date as "YYYYMMDD"
-                    String date = String.format("%04d%02d%02d", year, month, day);
-
-                    // Call saveHoroscopeDate with the formatted date
-                    luckService.saveLuckData(date);
-                }
-            }
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
-        return ResponseEntity.ok("OK");
-    }
 }
